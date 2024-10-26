c85e5b215a8462f7339957e86865e4e6b10b8527
Degarege Nibret <degarege3003nibret@gmail.com>
2024-10-26 23:32:27 +0300

    model updated

--- src/model_training.py
+++ src/model_training.py
@@ -4,17 +4,17 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 def train_logistic_regression(X_train, y_train):
-    model = LogisticRegression()
+    model = LogisticRegression(solver='saga', max_iter=200)
     model.fit(X_train, y_train)
     return model
 
 def train_random_forest(X_train, y_train):
-    model = RandomForestClassifier()
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
     return model
 
 def train_gradient_boosting(X_train, y_train):
-    model = GradientBoostingClassifier()
+    model = GradientBoostingClassifier(random_state=42)
     model.fit(X_train, y_train)
     return model
 
